705d86a4d66537ec3a3357596e5543d64b0359d1
Imanuel Sonubi <iman.sonubi@gmail.com>
2016-12-12 16:03:38 -0500

    Added functionality to view and paste saved reactions

--- ReactionSaver.js
+++ ReactionSaver.js
@@ -4,31 +4,54 @@ chrome.storage.sync.get("bookmarks", function(items) {
 		console.log("Bookmarks created");
 	} else {
 		console.log("Bookmarks already exist");
+		console.log(items.bookmarks);
 	}
 });
 
 chrome.contextMenus.create({
 	"type": "normal",
-	"id": "addImage",
-	"title": "Add this reaction",
+	"id": "addReaction",
+	"title": "Add this media",
 	"contexts": ["image"]
+});
+
+chrome.contextMenus.create({
+	"type": "normal",
+	"id": "openReactionWindow",
+	"title": "Insert reaction",
+	"contexts": ["editable"]
 });
 
 chrome.contextMenus.onClicked.addListener(reactionListener);
 
-function reactionListener(info) {
-	if (info.menuItemId === "addImage") {
-		chrome.storage.sync.get(function(items) {
-			var bookmarks = items.bookmarks;
-
-			bookmarks.push({
-				"url": info.srcUrl,
-				"timestamp": new Date().getTime()
-			});
-
-			console.log(bookmarks);
-
-			chrome.storage.sync.set( {"bookmarks": bookmarks} );
-		});
+function reactionListener(info, tab) {
+	if (info.menuItemId === "addReaction") {
+		addBookmark(info);
 	}
+
+	if (info.menuItemId === "openReactionWindow") {
+		viewBookmarks(tab);
+	}
+}
+
+function addBookmark(info) {
+	chrome.storage.sync.get(function(items) {
+		var bookmarks = items.bookmarks;
+
+		bookmarks.push({
+			"url": info.srcUrl,
+			"timestamp": new Date().getTime()
+		});
+
+		chrome.storage.sync.set( {"bookmarks": bookmarks} );
+	});
+}
+
+function viewBookmarks(tab) {
+	chrome.windows.create({
+		"url": "InsertReaction.html#" + tab.id,
+		"width": 600,
+		"height": 700,
+		"type": "popup"
+	});
 }
