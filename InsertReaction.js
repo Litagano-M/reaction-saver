f9dbeb71d47a7fcad9f9c4f8dfcab9d102607abf
Imanuel Sonubi <iman.sonubi@gmail.com>
2017-05-04 04:07:00 -0400

    1.1: Added functionality to mark bookmarks as favorites

--- InsertReaction.js
+++ InsertReaction.js
@@ -51,6 +51,28 @@ function removeBookmark(bookmarkToRemove) {
 	});
 }
 
+// Function for toggling favorites on bookmarks.
+function toggleFavorite(bookmarkToToggle) {
+	// Get the bookmarks from Chrome's synced storage.
+	chrome.storage.sync.get("bookmarks", function(items) {
+		var bookmarks = items.bookmarks; // Store the bookmark array in a variable so we can change it
+
+		// For each bookmark...
+		bookmarks.forEach(function(bookmark) {
+			// ...if the current bookmark's URL is equal to the URL of the bookmark passed into the function AND
+			//    if the current bookmark's timestamp is equal to the timestamp of the bookmark passed into the function...
+			if (bookmarkToToggle.url === bookmark.url &&
+				bookmarkToToggle.timestamp === bookmark.timestamp) {
+				// ...get its index and toggle its favorite value.
+				var indexToToggle = bookmarks.indexOf(bookmark);
+				bookmarks[indexToToggle].favorite = !bookmarks[indexToToggle].favorite;
+			}
+		});
+
+		chrome.storage.sync.set( {"bookmarks": bookmarks} ); // Store the array in the bookmarks variable back into Chrome's synced storage.
+	});
+}
+
 // Function for populating the list of bookmarks in InsertReaction.html.
 function populateList() {
 	// Get the bookmarks from Chrome's synced storage.
@@ -70,7 +92,7 @@ function populateList() {
 
 			// Create a remove button.
 			var $removeButton = $("<div>")
-				.addClass("remove-button") // Add the remove-button class
+				.addClass("button remove") // Add the button and remove classes (together, they make the remove button appearance)
 				.click(function(event) { // Add a click handler that passes the current bookmark to the removeBookmark() function and deletes the container node from the HTML page
 					event.stopPropagation();
 					removeBookmark(bookmark);
@@ -81,7 +103,37 @@ function populateList() {
 					function() { $(this).animate( {"opacity": "0.5"}, 100 ).dequeue(); }
 				);
 
+            // Create a favorite button.
+			var $favoriteButton = $("<div>")
+				.addClass("button favorite") // Add the button and favorite classes (together, they make the favorite button appearance)
+                .toggleClass("favorited", bookmark.favorite) // Toggle the "favorited" class that turns the button yellow depending on whether the bookmark is favorited or not
+				.click(function(event) { // Add a click handler that passes the current bookmark to the toggleFavorite function
+					event.stopPropagation();
+
+                    /* So this works non-intuitively. The "bookmark" variable passed into the callback function for the items.bookmarks.forEach() call way up above does not and is not affected by what's actually in storage. That is to say, if I were to later change anything about the bookmarks, it would not be reflected in the "bookmark" variable passed in. The "bookmark" variable only represents the bookmark storage as it was when it was queried at the chrome.storage.sync.get() call that was made when the user opened the "Insert Reaction" window.
+
+                    Therefore, I am using the statement below to toggle the favorite value in the "bookmark" variable in order to be able to correctly change the page's appearance. The toggleFavorite() function is the one that actually changes the favorite value in actual storage, so any changes to favorites persist.
+
+                    This is not perfect. The order in which the user sees their favorites as they change them and the order it will actually be in when the "Insert Reaction" window renders will be different, which is apparent when the user closes and reopens the window or refreshes it. Perhaps there's a better way of doing this. I'll have to see about it later. */
+
+                    bookmark.favorite = !bookmark.favorite; // Toggle the favorite key of the current bookmark's representation in the "bookmark" variable
+                    toggleFavorite(bookmark); // This is the function that actually toggles the bookmark's favorite value in the storage by passing in the "bookmark" variable and finding the bookmark in storage with that
+
+                    if (bookmark.favorite) { // If the "bookmark" variable's favorite value is true...
+                        $container.detach().appendTo($("#favorites-collection")); // ...then detach the container from the regular collection div and place it in the favorites collection div
+                    } else {
+                        $container.detach().appendTo($("#regular-collection")); // ...else detach the container from the favorites collection div and place it in the regular collection div
+                    }
+
+                    $(this).toggleClass("favorited", bookmark.favorite); // Toggles the "favorited" class responsible for changing the color of the button
+				})
+				.hover( // Add a hover handler that animates the opacity of the button
+					function() { $(this).animate( {"opacity": "1"}, 100 ).dequeue(); },
+					function() { $(this).animate( {"opacity": "0.5"}, 100 ).dequeue(); }
+				);
+
 			$container.append($removeButton); // Append the remove button to the container
+            $container.append($favoriteButton); // Append the favorite button to the container
 			$container.append($image); // Append the image to the container
 
             // If the bookmark is a favorite...
